07a1ddf5d0f721f35e687b15aec6d4959de1dc92
tenshine <tanwnx@outlook.com>
2020-01-14 02:12:26 +0800

    finish

--- clientend/client.go
+++ clientend/client.go
@@ -8,7 +8,6 @@ import (
 	"io/ioutil"
 	"log"
 	"net/http"
-	"os"
 	"strconv"
 	"syscall"
 	"time"
@@ -54,21 +53,17 @@ func autoKill(processes []watch.GpuProcess) {
 			if p.MemoryUsage >= *upbound {
 				pl[key] = val + 1
 
-				if val+1 == 3 {
-					// err := syscall.Kill(int(p.PID), syscall.SIGKILL)
-					// if err != nil {
-					// 	log.Panic("Kill failed ", err)
-					// }
-					ps, _ := os.FindProcess(int(p.PID))
-					fmt.Println(ps)
+				if val+1 == *times {
+					err := syscall.Kill(int(p.PID), syscall.SIGKILL)
+					if err != nil {
+						log.Panic("Kill failed ", err)
+					}
 					log.Println("Killed the Process " + strconv.Itoa(int(p.PID)) + "Name: " + p.Name + "Memory usage: " + strconv.Itoa(p.MemoryUsage) + "%")
 					_, ok := pl[key]
 					if ok {
 						delete(pl, key)
 					}
 				}
-			} else {
-				pl[key] = val
 			}
 
 		} else {
@@ -94,6 +89,7 @@ var addr = flag.String("addr", "None", "server address, if not set this, it will
 var hostname = flag.String("hostname", "ts_client", "current hostname something like `gpu0`")
 var upbound = flag.Int("upbound", 80, "the upbound of process gpu memoory usage, default 80.")
 var interval = flag.Uint("interval", 3, "default 3, which means scan all gpu process every 3s")
+var times = flag.Int("Times", 60, "the maximum number of consecutive times, default 60")
 
 func main() {
 	initFlag()
